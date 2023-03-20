ec2674b08bc20b91ff01113a31440d5cdd6cce5d
DiceglieA <angelodiceglie10@gmail.com>
2023-03-20 16:59:57 +0100

    definiti i prodotti

---
+++ Product.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private float prezzo;
+        private float iva;
+        public Prodotto(string nome, string descrizione, float prezzo, float iva)
+        {
+            this.codice = generaCodice();
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+        
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public void setDescrizione(string descrizione)
+        {
+            this.descrizione = descrizione;
+        }
+        public string getDescrizione()
+        {
+            return descrizione;
+        }
+
+        public void setPrezzo(float prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+        public float getPrezzo()
+        {
+            return prezzo;
+        }
+
+        public void setIva(float iva)
+        {
+            this.iva = iva;
+        }
+        public float getIva()
+        {
+            return iva;
+        }
+
+        public int getCodice()
+        {
+            return codice;
+        }
+        private int generaCodice()
+        {
+            Random random = new Random();
+            return random.Next(10000);
+        }
+
+        override
+        public string ToString()
+        {
+            return "Prodotto:\n" +
+                    "Codice: " + creaCodice() + "\n" +
+                    "Nome: '" + nome + "'\n" +
+                    "Descrizione: '" + descrizione + "'\n" +
+                    "Prezzo: " + prezzo + "\n" +
+                    "Prezzo con Iva: " + prezzoIva() + "\n";
+        }
+
+        public float prezzoIva()
+        {
+            return prezzo + (prezzo * iva);
+        }
+
+        public string nomeEsteso()
+        {
+            return "Nome esteso:" + codice + " - " + nome + "\n";
+        }
+
+        private string creaCodice()
+        {
+            string result = "";
+
+            result = codice.ToString();
+            for (int i = 0; i < 8; i++)
+            {
+                if (result.Length < 8)
+                    result = "0" + result;
+            }
+            
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿//Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+//Un prodotto è caratterizzato da:
+//-codice(numero intero)
+//- nome
+//- descrizione
+//- prezzo
+//- iva
+//Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+//Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interargirci per testare tutti i metodi che avete previsto.
+using csharp_oop_shop;
+
+Prodotto penna = new Prodotto("Penna", "Penna nera", 3, 0.23f);
+Prodotto latte = new Prodotto("Latte", "Bevanda", 2, 0.04f);
+Prodotto macchinina = new Prodotto("Giocattolo", "Macchinina radiocomandata", 10, 0.23f);
+
+Console.WriteLine(penna.ToString());
+Console.WriteLine(latte.ToString());
+Console.WriteLine(macchinina.ToString());
+
+Console.WriteLine(penna.nomeEsteso());
+Console.WriteLine(latte.nomeEsteso());
+Console.WriteLine(macchinina.nomeEsteso());
